7283231fbed054d25bdf7f21593fb8c355c45496
Hyun Soo <shyunsu3@gmail.com>
2026-04-13 10:21:10 +0900

    chore:update_entrie_pipeline.py

--- update_entrie_pipeline.py
+++ update_entrie_pipeline.py
@@ -8,32 +8,35 @@ if PROJECT_ROOT not in sys.path:
 import pandas as pd
 
 from src.parsing.concat import run_full_pipeline
-from src.preprocessing.metadata_cleaning import process_metadata
-from src.pipeline.rag_pipeline import meta_pipeline
-from src.ingestion.chunker import process_chunking
+from src.pipeline.rag_pipeline import meta_pipeline, chunk_pipeline
 from src.embedding.vector_store import build_and_save_db
 
-def update_entrie_pipeline():
+from pathlib import Path
+from dotenv import load_dotenv
+from config import DATABASE_DIR, OUTPUT_DIR
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+load_dotenv(ROOT_DIR / ".env")
+
+def update_entrie_pipeline(use_cleaning: bool = True, use_meta_prefix: bool = True):
     print("="*50)
     print("[Bid Coin] 전체 데이터 파이프라인 업데이트 시작...")
     print("="*50)
-
-    # 경로 설정
-    SHARED_FILES_DIR = "/home/shared/files"
-    OUTPUT_DIR = "/home/bidcoin"
+    
+    # 불러오기
+    df = pd.read_csv(DATABASE_DIR / "data_list.csv", encoding="utf-8")
 
     # [1] PDF/HWP 파싱 → 병합
     print("\n PDF 및 HWP 파싱 진행중...")
-    df_parsed = run_full_pipeline(folder_path=SHARED_FILES_DIR, output_dir=OUTPUT_DIR)
+    df_parsed = run_full_pipeline(folder_path=DATABASE_DIR / "files", output_dir=OUTPUT_DIR)
 
     # [2] 메타데이터 정제 및 재파싱
     print("\n 메타데이터 정제 및 재파싱 진행중...")
-    OPTION = True
-    df_meta = meta_pipeline()
+    df_meta = meta_pipeline(use_cleaning=use_cleaning)
 
     # [3] 텍스트 청킹 및 메타데이터 결합 진행
     print("\n 텍스트 청킹 및 메타데이터 결합 진행...")
-    chunked_df = process_chunking(df_parsed, df_meta)
+    df_chunked = chunk_pipeline(df_parsed, df_meta, use_meta_prefix=use_meta_prefix)
 
     # [4] FAISS 벡터 스토어 구축 및 저장
     print("\n 벡터 스토어 구축 및 저장 진행중...")
